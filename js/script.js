4b0d8c4b61bfa26e12fe7a0f033a416e95d3ec45
Dima Vavilov <dv@softevol.com>
2022-01-08 14:39:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('%cDeveloped by Djigit & yur4ik96', 'color: #c434c4; font-size: 50px ');
+
+console.log(
+  '%cTG: @Djigit34 / @yura_k18',
+  'color: #c434c4; background: #eee; font-size: 30px '
+);
+
+
+const headerNav = document.querySelector(".header__nav-container")
+const headerNavOpener = document.querySelector(".header__nav-opener")
+
+headerNavOpener.addEventListener("click",()=>{
+  headerNav.classList.contains("active") ? headerNav.classList.remove("active") : headerNav.classList.add("active")
+  headerNav.classList.contains("active") ? headerNavOpener.classList.add("active") : headerNavOpener.classList.remove("active")
+})
