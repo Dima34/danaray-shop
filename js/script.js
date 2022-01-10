dd23ccb1285f38f9132780cda4559125b899098e
Dima34 <57358582+Dima34@users.noreply.github.com>
2022-01-10 08:59:14 +0200

    header fix

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ console.log(
   'color: #c434c4; background: #eee; font-size: 30px '
 );
 
+const body = document.querySelector("body")
 
 const headerNav = document.querySelector(".header__nav-container")
 const headerNavOpener = document.querySelector(".header__nav-opener")
 
+
 headerNavOpener.addEventListener("click",()=>{
   headerNav.classList.contains("active") ? headerNav.classList.remove("active") : headerNav.classList.add("active")
   headerNav.classList.contains("active") ? headerNavOpener.classList.add("active") : headerNavOpener.classList.remove("active")
+  headerNav.classList.contains("active") ? body.classList.add("no-scroll") : body.classList.remove("no-scroll")
 })
